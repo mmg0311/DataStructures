06dea391990c4837652e30345ffe6f0a70ad516b
Mohak Gadge <mohakgadge@gmail.com>
2020-03-23 19:23:28 +0530

    minHeap cpp program
    minHeap cpp program  with all basic functions  along with additional delete function

---
+++ minHeap.cpp
@@ -0,0 +1,133 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class heap{
+    int *arr;
+    int size;
+    int heap_ind;
+    public:
+      heap(int s){
+        size = s;
+        arr = new int[s];
+        heap_ind = 0;
+      }
+      int getParentIndex(int i){
+        return ((i-1)/2);
+      }
+      int getLeftChildIndex(int i){
+        return (i*2+1);
+      }
+      int getRightChildIndex(int i){
+        return (i*2+2);
+      }
+      int swap(int i1,int i2){
+        int temp = arr[i1];
+        arr[i1] = arr[i2];
+        arr[i2] = temp;
+      }   
+      bool hasLeftChild(int ind){
+        if(getLeftChildIndex(ind) < size){
+          return true;
+        }
+        return false;
+      }
+      bool hasRightChild(int ind){
+        if(getRightChildIndex(ind) < size){
+          return true;
+        }
+        return false;
+      }
+      bool hasParent(int ind){
+        if(getParent(ind) >= 0){
+          return true;
+        }
+        return false;
+      }
+      void HeapifyUp(int ind);
+      void HeapifyDown();
+      bool insert(int data);
+      int getMinEle();
+      bool removeTop();
+      bool deleteElement(int data);
+      bool deleteElementAtIndex(int index);
+}
+
+int heap::getMinEle(){
+  if(heap_ind == 0){
+    return -1;
+  }
+  return arr[0];
+}
+
+bool heap::removeTop(){
+  if(heap_ind == 0){
+    return  false;
+  }
+  arr[0] = arr[heap_ind-1];
+  heap_ind--;
+  HeapifyDown();
+}
+
+bool heap::insert(int data){
+   if(heap_ind > size){
+      //error  
+      return false;
+   }
+   arr[heap_ind] = data;
+   HeapifyUp(heap_ind);
+   heap_ind++;
+}
+
+void HeapifyUp(int ind){
+  int index = ind ;
+  while(hasParent(index) && arr[getParentIndex(index)] > arr[index]){
+    swap(getParentIndex(index),index);
+    index = getParentIndex(index);
+  }
+}
+
+void HeapifyDown(){
+  int index = 0;
+  while(hasLeftChild(index) ){
+    int small = getLeftChild(index);
+    if(hasRightChild(index) && arr[small] > arr[getRightChild(index)]){
+        small = getRightChild(index);  
+    }
+    if(arr[index] > arr[small]){
+      swap(small,index); 
+    }else{
+      break;
+    }
+    index = small;
+  }
+}
+
+bool heap::deleteElementAtIndex(int index){
+  if(index < heap_ind){
+    arr[index] = INT_MIN;
+    heapifyUp(index);
+    removeTop();
+    return true;
+  }
+  return false;
+}
+
+bool heap::deleteElement(int data){
+    for(int i = 0; i < heap_ind ; i++){
+      if(arr[i] == data){
+         return deleteElementAtIndex(i);
+      }
+    }
+    return false;
+}
+
+int main(){
+    heap h(10);
+    h.insert(2);
+    h.insert(3);
+    h.insert(4);
+    h.insert(1);
+    cout << h.getMinEle();
+    h.deleteElement(3);
+    
+}

61a5bc63cdfefca44a820bc316ee66fd864619e7
mmg0311 <mohakgadge@gmail.com>
2020-03-26 10:45:40 +0530

    all singly linked list solutions

--- linkedList/list.cpp
+++ linkedList/list.cpp
@@ -159,6 +159,106 @@ int compare(struct node *head1, struct node *head2)
 //merge two sorted linked list
 struct node *mergeLists(node *head1, node *head2)
 {
+   struct node *result = NULL;
+   if (head1 == NULL)
+   {
+      return head2;
+   }
+   if (head2 == NULL)
+   {
+      return head1;
+   }
+   if (head1->data <= head2->data)
+   {
+      result = head1;
+      result->next = mergeLists(head1->next, head2);
+   }
+   else
+   {
+      result = head2;
+      result->next = mergeLists(head1, head2->next);
+   }
+   return result;
+}
+
+//delete duplicate value nodes from the sorted list
+struct node *deleteDuplicate(struct node *head)
+{
+   struct node *curr = head;
+   struct node *next = head->next;
+   struct node *copy = NULL;
+   while (curr->next != NULL && next != NULL)
+   {
+      if (curr->data == next->data)
+      {
+         copy = next;
+         next = next->next;
+         curr->next = next;
+         delete (copy);
+      }
+      else
+      {
+         curr = curr->next;
+         next = curr->next;
+      }
+   }
+   return head;
+}
+
+//get node from tail of the list i.e pos == 0 means tail node
+//pos == 1 means second last node ....
+int getNodeFromTail(struct node *head, int pos)
+{
+   struct node *h = head;
+   struct node *h2 = head;
+   pos++;
+   while (pos--)
+   {
+      h2 = h2->next;
+   }
+   while (h2 != NULL)
+   {
+      h = h->next;
+      h2 = h2->next;
+   }
+   return h->data;
+}
+
+bool hasCycle(struct node *head)
+{
+   struct node *curr = head, *next = head;
+   while (curr != NULL && next != NULL && next->next != NULL)
+   {
+      curr = curr->next;
+      next = next->next->next;
+      if (curr->data == next->data)
+      {
+         return true;
+      }
+   }
+   return false;
+}
+
+//find merge node in the lists
+int findMergeNode(struct node *head1, struct node *head2)
+{
+   unordered_map<struct node *, int> m;
+   struct node *h = head1;
+   while (h != NULL)
+   {
+      m[h] = 1;
+      h = h->next;
+   }
+   h = head2;
+   while (h != NULL)
+   {
+      if (m[h] == 1)
+      {
+         return h->data;
+      }
+      h = h->next;
+   }
+   return -1;
 }
 
 int main()
